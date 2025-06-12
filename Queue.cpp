b5cd8f09f1eeda2ccbef51206211025d90a34afe
ilham101106 <pesatoffical321@gmail.com>
2025-06-12 14:47:09 +0700

    membuat class queues

--- Queue.cpp
+++ Queue.cpp
@@ -11,27 +11,6 @@ using namespace std;
 class Queues
 {
 
-private:
-    static const int max = 5;
-    int FRONT, REAR;
-    int queue_array[5];
-    int FRONT, REAR, N = 5;
-    int Queue_array[5];
-    
-public:
-    Queues(){
-        FRONT = -1;
-        REAR = -1;
-
-    }
-
-    void insert()
-    {
-        int num;
-        cout<<"Enter a number:";
-        cin>>num;
-        cout<<endl;
-    }
 
     
 };
