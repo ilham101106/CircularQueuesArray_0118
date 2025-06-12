73a7fc99c257370732cd7133930f1199549107b1
ilham101106 <pesatoffical321@gmail.com>
2025-06-12 15:54:15 +0700

    menambahkan menu pilihan  pada fungsi utama

--- Queue.cpp
+++ Queue.cpp
@@ -142,4 +142,16 @@ int main()
 
     while (true)
     {
-    
+        try
+        {
+            cout<<"Menu"<<endl;
+            cout<<"1. Implement insert operation"<<endl;
+            cout<<"2. Implement delete opereation"<<endl;
+            cout<<"3. Display values"<<endl;
+            cout<<"4. Exit"<<endl;
+            cout<<"Enter your delete (1-4)"<<endl;
+            cin>>ch;
+            cout<<endl;
+
+
+          
