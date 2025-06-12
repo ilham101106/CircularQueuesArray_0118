8fa22f704ceee7dee9192375ae79eafcd4fb84ea
ilham101106 <pesatoffical321@gmail.com>
2025-06-12 14:56:19 +0700

    mengecek apakah antrian penuh

--- Queue.cpp
+++ Queue.cpp
@@ -11,6 +11,42 @@ using namespace std;
 class Queues
 {
 
+private:
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+    int FRONT, REAR, N = 5;
+    int Queue_array[5];
+    
+public:
+    Queues(){
+        FRONT = -1;
+        REAR = -1;
+
+    }
+
+    void insert()
+    {
+        int num;
+        cout<<"Enter a number:";
+        cin>>num;
+        cout<<endl;
+
+
+        //1. cek apakah antrian penuh
+
+        if ((FRONT == 0 && REAR == max -1 ) ||(FRONT == REAR + 1))
+        {
+            cout<<"\nQueue overflow\n"; //.a
+            return;                     //.b
+        }
+
+
+    }
+
+    
+   
+    
 
     
 };
